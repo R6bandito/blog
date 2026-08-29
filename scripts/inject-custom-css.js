10933fabe5559622f817c4301da9a6ed68f3a8fb
R6bandito <3341444569@qq.com>
2026-08-29 16:42:41 +0800

    页面切换优化

--- scripts/inject-custom-css.js
+++ scripts/inject-custom-css.js
@@ -2,3 +2,6 @@
 // 放在主题 default.css 之后加载，可覆盖主题默认样式
 // Hexo 8 已移除 _config.yml 中的 inject 配置支持，故使用 injector API
 hexo.extend.injector.register('head_end', '<link rel="stylesheet" href="/css/custom.css">');
+
+// 提前预加载背景图，缩短整页加载时的白屏时间
+hexo.extend.injector.register('head_begin', '<link rel="preload" as="image" href="/images/bg.webp">');
